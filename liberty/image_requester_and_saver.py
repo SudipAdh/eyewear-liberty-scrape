58d02b86fbe1931e157ffb4cbf774ee0e79571bc
Sudip ADh <sudipadh@Sudips-MacBook-Pro-M1.local>
2021-12-22 22:35:30 +0545

    jpeg to jpg

--- liberty/image_requester_and_saver.py
+++ liberty/image_requester_and_saver.py
@@ -5,10 +5,10 @@ import boto3
 
 df = pd.read_csv("../liberty_import.csv")
 
-jpeg_files = glob("./image_work/*.jpeg")
+jpeg_files = glob("./image_work/*.jpg")
 def requester_and_uploader(x):
     if x and not isinstance(x, float):
-        file_name = "./image_work/"+x.rsplit("/", 1)[-1].split(".")[0]+".jpeg"
+        file_name = "./image_work/"+x.rsplit("/", 1)[-1].split(".")[0]+".jpg"
         if not "./image_work/"+file_name in jpeg_files:
             resp = requests.get(x, stream=True)
             file = open(file_name, "wb")
